7e0fa41c32a553a3d82388767de7b9c62d9832f4
daniele-rosati <drosati151@gmail.com>
2024-03-11 17:52:15 +0100

    struttura

--- array numeri/script.js
+++ array numeri/script.js
@@ -1,5 +1,28 @@
-// Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
+// Chiedere all'utente 5 numeri. 
+//Inserire i numeri nell'array
+// ma senza creare duplicati 
+//(inserire il numero nell'array solo se non è già presente).
 
 // creo una arrey vuoto
- let numbers
+ const numbers = [];
+
+ // chiedo all'utente di inserire un numero
+
+// let askN = [];
+
+ // chiedo all'untente di inserire 5 numeri
+
+ for (let i=0; i<5; i++){
+    let newN = askN();
+    if(!numbers.includes (newN)){
+        numbers.push(newN);
+    } else {
+        alert ("ATTENZIONE, questo numero è già stato inserito")
+    }
+ }
+
+ // stampo l'arrey
+
+ console.log("I numeri inseriti sono: ", numbers)
+
 
