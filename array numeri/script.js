1fa8fc303c095daedd7cd0b4cd93cb98e32cfb8e
daniele-rosati <drosati151@gmail.com>
2024-03-11 17:37:20 +0100

    cartella array numeri + struttura

---
+++ array numeri/script.js
@@ -0,0 +1,5 @@
+// Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
+
+// creo una arrey vuoto
+ let numbers
+
